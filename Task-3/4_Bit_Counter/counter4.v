e501998f3726dc9a959cd91ddf899c67a8514842
Charan141205 <lagisettyvidyacharan141205@gmail.com>
2026-08-11 15:02:46 +0530

    Add 4-bit counter module with reset functionality

---
+++ Task-3/4_Bit_Counter/counter4.v
@@ -0,0 +1,15 @@
+module counter4(
+    input clk,
+    input reset,
+    output reg [3:0] count
+);
+
+always @(posedge clk)
+begin
+    if (reset)
+        count <= 4'b0000;
+    else
+        count <= count + 1'b1;
+end
+
+endmodule
